75ef618ec90f245ef5af4d2505f3ca1c3a2e877f
Lev1ster <amka.feremina@yandex.ru>
2023-03-27 16:10:24 +0300

    HH_Task_1

--- FindArea/FiguresTests/FigureArea_Tests.cs
+++ FindArea/FiguresTests/FigureArea_Tests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Figures;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,24 @@ namespace FiguresTests
 {
     public class FigureArea_Tests
     {
+        [TestCase(0)]
+        [TestCase(1)]
+        public void FindAreaFigure_Success(int i)
+        {
+            //Act
+            double expected = DataForArea.data[i].FindArea();
+            var figure = DataForArea.data[i];
+
+            //Arrange
+            double actual = FigureArea.FindArea(figure);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    public static class DataForArea
+    {
+        public static IFigure[] data = new IFigure[2] { new Triangle(2, 3, 2), new Circle(3) };
     }
 }
